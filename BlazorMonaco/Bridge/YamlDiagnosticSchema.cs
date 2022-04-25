8b81d99b66a021b38b9998d51cea3d09333e486c
Quentin Metge <quentin.metge@ubisoft.com>
2022-04-25 11:02:04 +0200

    simplify helpers

--- BlazorMonaco/Bridge/YamlDiagnosticSchema.cs
+++ BlazorMonaco/Bridge/YamlDiagnosticSchema.cs
@@ -2,6 +2,12 @@
 {
     public class YamlDiagnosticSchema
     {
+        public YamlDiagnosticSchema(string uri, params string[] fileMatch)
+        {
+            Uri = uri;
+            FileMatch = fileMatch;
+        }
+
         public string Uri { get; set; }
 
         public string[] FileMatch { get; set; } 
